8ecbff325c46943a1d8fd487dbf2e8bb47e2d4d4
Ashmeet Singh <ashmeet@example.com>
2025-08-29 13:44:50 +0530

    port solving changes

--- server.js
+++ server.js
@@ -12,8 +12,13 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// ✅ Completely public CORS
+app.use(cors({
+  origin: "*",
+  credentials: true, // allow cookies, tokens
+}));
+
 // Middleware
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
